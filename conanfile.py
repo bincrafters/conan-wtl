e7872d9957d56bbf9b6148d387db26814b59c0f7
Uilian Ries <uilianries@gmail.com>
2020-02-06 09:41:28 -0300

    Bump version to 10.0.7336
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,26 @@
+import os
 from conans import ConanFile, tools
 
 
 class WTLConan(ConanFile):
     name = "wtl"
-    version = "10.0.7336"
     url = "https://github.com/bincrafters/conan-wtl"
     description = "Windows Template Library (WTL) is a C++ library for developing Windows applications and UI " \
                   "components. It extends ATL (Active Template Library) and provides a set of classes for controls, " \
                   "dialogs, frame windows, GDI objects, and more."
+    homepage = "https://wtl.sourceforge.io"
+    topics = ("template", "windows-template-library", "windows", "atl", "gdi")
     no_copy_source = True
     license = "Common Public License"
     exports = ["LICENSE.md"]
     settings = {'os': ['Windows']}
 
     def source(self):
-        major, _, build = self.version.split('.')
-        source_url = "https://datapacket.dl.sourceforge.net/project/wtl/WTL%20{major}/WTL%20{version}/" \
-                     "WTL{major}_{build}.zip".format(major=major, build=build, version=self.version)
-        tools.get(source_url)
+        tools.get(**self.conan_data["sources"][self.version])
 
     def package(self):
-        self.copy(pattern="MS-PL.TXT", dst="license", src=".")
-        self.copy(pattern="*", dst="include", src="Include")
+        self.copy(pattern="MS-PL.TXT", dst="license", src=self.source_folder)
+        self.copy(pattern="*", dst="include", src=os.path.join(self.source_folder, "Include"))
 
     def package_id(self):
         self.info.header_only()
